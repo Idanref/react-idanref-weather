5090eae12aac261ce116bec0189d317dfc6eea4b
Idan Refaeli <idanref@gmail.com>
2021-10-05 05:15:52 +0300

    added CI= for netlify deploying

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LeftInfo from './components/LeftInfo.js';
 import Sidebar from './components/Sidebar';
 
 // Add photos to github
+// fixed live clock, added dynamic backgrounds, added dynamic weather icons
 
 function App() {
   return (
